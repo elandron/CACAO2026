c96248c3c854cae515b0e0e60e070f1bd5899dc5
Ewan Lefort <ewan.lefort@imt-atlantique.net>
2026-03-24 10:13:25 +0100

    stock avec choco marque mq

--- src/abstraction/eq4Transformateur1/Transformateur1Stock.java
+++ src/abstraction/eq4Transformateur1/Transformateur1Stock.java
@@ -6,16 +6,27 @@ import java.util.*;
 import abstraction.eqXRomu.produits.Feve;
 import abstraction.eqXRomu.produits.IProduit;
 import abstraction.eqXRomu.produits.Chocolat;
+import abstraction.eqXRomu.produits.ChocolatDeMarque;
 
 public class Transformateur1Stock extends Transformateur1Acteur{
 
     private HashMap<IProduit, Double> stock;
+    private ChocolatDeMarque ProntellaM= new ChocolatDeMarque(Chocolat.C_MQ, "ProntellaM", 65);
 
     public Transformateur1Stock(){
         super();
         this.stock=new HashMap<IProduit, Double>();
     }
-
+    public List<ChocolatDeMarque> getChocolatsProduits(){
+		List<ChocolatDeMarque> ListeChoco=new ArrayList<ChocolatDeMarque>();
+		ListeChoco.add(ProntellaM);
+		return ListeChoco;
+	}
+	public List<String> getMarquesChocolat(){
+		List<String> ListeNoms= new ArrayList<String>();
+		ListeNoms.add("ProntellaM");
+		return ListeNoms;
+	}
     public void initialiser(){
         this.stock.put(Feve.F_BQ,0.0);
         this.stock.put(Feve.F_MQ,0.0);
@@ -29,6 +40,7 @@ public class Transformateur1Stock extends Transformateur1Acteur{
         this.stock.put(Chocolat.C_BQ_E,0.0);
         this.stock.put(Chocolat.C_MQ_E,0.0);
         this.stock.put(Chocolat.C_HQ_E,0.0);
+        this.stock.put(ProntellaM,0.0);
     }
 
     public HashMap<IProduit, Double> getStock(){
@@ -57,8 +69,17 @@ public class Transformateur1Stock extends Transformateur1Acteur{
         return totalstock;
     }
 
+    public double getTotalStocksChocoMarque(){
+        double totalstock=0;
+        for (IProduit choco: stock.keySet()){
+            if (choco.getType()=="ChocolatDeMarque");
+            totalstock+=this.getStocksProduit(choco);
+        }
+        return totalstock;
+    }
+
     public double getTotalStocks(){
-        return this.getTotalStocksChoco()+this.getTotalStocksFeves();
+        return this.getTotalStocksChoco()+this.getTotalStocksFeves()+this.getTotalStocksChocoMarque();
     }
 
    
